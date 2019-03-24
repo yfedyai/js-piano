4d23ee23f39666ea4090bbc6609f8b23310100d0
Yarik Fedyai <yarkofedyai@gmail.com>
2019-03-24 16:37:15 +0200

    create js piano

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+
+const playMusic = (elem) => {
+    elem.onended = () => elem.parentNode.classList.remove('play');
+    elem.play();
+};
+
+const stopMusic = (elem) => {
+    elem.pause();
+};
+
+
+const playStop = (elem) => {
+    elem.classList.toggle('play');
+    elem.classList.contains('play')
+        ? playMusic(elem.firstElementChild)
+        : stopMusic(elem.firstElementChild);
+}
+
+
+
+let pianoKey = document.querySelectorAll('.piano-button');
+
+pianoKey.forEach(item => {
+    console.log(item.firstElementChild);
+    item.addEventListener('click', () => {
+        playStop(item);
+    })   
+});
+
+
+
+
+document.addEventListener('keydown', (key) => {
+    console.log(key.keyCode)
+    console.log();
+    let buttonDown = key.keyCode;
+    let playElement = document.querySelector(`div[data-key="${buttonDown}"]`);
+    console.log('playElement: ', playElement);
+    playStop(playElement)
+   
+})
